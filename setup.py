1a9526483835c7b05bbdadccbea6d03c4e89265d
Balz Aschwanden <balz.aschwanden@unibas.ch>
2019-11-28 14:22:19 +0100

    Fix requirements for python 3

--- setup.py
+++ setup.py
@@ -52,5 +52,5 @@ setuptools.setup(
         "antslib.inventory": ["inventory_default", "inventory_ad"],
         "antslib.plugins": ["callback/ants_logstash.py"],
     },
-    python_requires=">=2.7,>=3.6",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
 )
