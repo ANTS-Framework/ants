96ae24a5203922a4ae0e89215d345cd6687dd9c7
koetim01 <tim.koenigl@unibas.ch>
2020-02-03 15:53:40 +0100

    Extend Pre run checker
    * Added a config variable <known_hosts_file>
    * Added a check if the hostname of the given git repository does exist in the known hosts file.

--- antslib/pre_run_checker.py
+++ antslib/pre_run_checker.py
@@ -2,10 +2,12 @@
 ==================
 Check different requirements necessary for ants
 """
+import os
 import subprocess
 import sys
+import urllib.parse as up
 
-from antslib import logger
+from antslib import logger, configer
 
 
 def check_git_installed():
@@ -20,7 +22,25 @@ def check_git_installed():
 
 
 def check_known_host():
-    pass
+    CFG = configer.read_config("main")
+    known_hosts_file = CFG["known_hosts_file"]
+
+    if os.path.isfile(known_hosts_file):
+        file = open(known_hosts_file, "r")
+        git_repo = CFG["git_repository"]
+        hostname = (up.urlparse(git_repo)).hostname
+
+        if hostname in file.read():
+            logger.console_logger.info(
+                f"Hostname {hostname} for git-repository {git_repo} is added in {known_hosts_file}"
+            )
+        else:
+            sys.exit(
+                f"Hostname {hostname} for git-repository {git_repo} is not added in {known_hosts_file}"
+            )
+
+    else:
+        sys.exit(f"Known hosts file {known_hosts_file} does not exists.")
 
 
 def check_ssh_key():
